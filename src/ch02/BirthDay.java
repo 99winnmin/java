2735245f9b088660535c126a557a23e5ff0d873d
seungmin Ryu <ojysep9987@naver.com>
2022-06-30 12:17:25 +0900

    JDK 버전 이슈로 인한 주석처리

--- src/ch02/BirthDay.java
+++ src/ch02/BirthDay.java
@@ -46,21 +46,21 @@ public class BirthDay {
                     break;
             }*/
             //switch 신버전(~java 14)
-            int day = switch (month) {
-                case 1, 3, 5, 7, 8, 10,12 -> {
-                    yield 31;
-                }
-                case 4,6,9,11 -> {
-                    yield 30;
-                }
-                case 2 ->{
-                    yield 28;
-                }
-                default->{
-                    yield 0;
-                }
-            };
-            setDay(day);
+//            int day = switch (month) {
+//                case 1, 3, 5, 7, 8, 10,12 -> {
+//                    yield 31;
+//                }
+//                case 4,6,9,11 -> {
+//                    yield 30;
+//                }
+//                case 2 ->{
+//                    yield 28;
+//                }
+//                default->{
+//                    yield 0;
+//                }
+//            };
+//            setDay(day);
         }
     }
 
