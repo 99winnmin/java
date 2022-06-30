3d2317ef68386ae743401aa844fd9fdd72a55043
seungmin Ryu <ojysep9987@naver.com>
2022-06-30 13:02:04 +0900

    형변환 개념

--- src/ch03/extends2/CustomerTest.java
+++ src/ch03/extends2/CustomerTest.java
@@ -14,5 +14,10 @@ public class CustomerTest {
         customerKim.bonusPoint = 100000;
         System.out.println(customerKim.showCustomerInfo());
 
+        Customer vc = new VIPCustomer(3,"third");
+        //vc.getAgentID(); ->ERROR
+        //VIPCustomer() 생성자에 의해 VIPCustomer 클래스의 모든 멤버 변수에 대한 메모리는 생성되었지만,
+        //변수의 타입이 Customer 이므로 실제 접근 가능한 변수나 메서드는 Customer의 변수와 메서드임
+        System.out.println(vc.showCustomerInfo());
     }
 }
